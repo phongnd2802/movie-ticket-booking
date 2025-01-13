24dd3128ecb932bb74a20888437d8946670a00a5
truongsondev <sonltute@gmail.com>
2025-01-13 17:09:21 +0700

    set front and primary color

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -1,11 +1,11 @@
 import "./globals.css";
-
+import { lato } from "@/ui/fonts";
 function RootLayout({ children }) {
   return (
-    <html>
-      <body>
+    <html lang="en">
+      <body className={`${lato.className} antialiased`}>
         <h1>this is layout root</h1>
-        <>{children}</>
+        {children}
       </body>
     </html>
   );
