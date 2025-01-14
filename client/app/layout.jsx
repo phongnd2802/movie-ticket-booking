7c8eee8435587490fdfc1a85b51e760b0c24610b
truongsondev <sonltute@gmail.com>
2025-01-14 13:16:42 +0700

    implement home page

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -3,7 +3,7 @@ import { lato } from "@/ui/fonts";
 function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${lato.className} antialiased`}>
+      <body className={`${lato.className} antialiased bg-white`}>
         <main className="m-auto ">{children}</main>
       </body>
     </html>
--- client/components/ui/header.jsx
+++ client/components/ui/header.jsx
@@ -3,32 +3,44 @@ import Image from "next/image";
 import clsx from "clsx";
 import Link from "next/link";
 
+import DropDownMenu from "./drop-down-menu";
+
+const data = [
+  { href: "/news", text: "sex" },
+  { href: "/login", text: "qmh" },
+  { href: "/regiter", text: "javhd" },
+];
+
 function Header() {
   return (
-    <div className={clsx("w-full p-[12px] bg-white")}>
-      <div className={clsx("w-[80%] m-auto flex justify-between items-center")}>
-        <div>
-          <Image src="/image/galaxy-logo.png" width={115} height={60} />
-        </div>
-        <div>
-          <h2>adkjfhdjssdhf</h2>
-        </div>
-        <div className="flex gap-5 items-center text-sm text-[#777]">
+    <header>
+      <div className={clsx("w-full p-[12px] bg-white")}>
+        <div
+          className={clsx("w-[80%] m-auto flex justify-between items-center")}
+        >
           <div>
-            <label>
-              <Search />
-            </label>
+            <Image src="/image/galaxy-logo.png" width={115} height={60} />
           </div>
           <div>
-            <span>
-              <Link href="/login" className="text-sm ">
-                Đăng nhập
-              </Link>
-            </span>
+            <DropDownMenu title="Phim" lists={data} />
+          </div>
+          <div className="flex gap-5 items-center text-sm text-[#777]">
+            <div>
+              <label>
+                <Search />
+              </label>
+            </div>
+            <div>
+              <span>
+                <Link href="/login" className="text-sm ">
+                  Đăng nhập
+                </Link>
+              </span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </header>
   );
 }
 
