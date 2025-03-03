6766127331877f9f062f5a0d9580e0122e9feef3
truongsondev <sonltute@gmail.com>
2025-03-03 20:46:13 +0700

    edit environment

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -22,7 +22,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.4.1")
     implementation("org.postgresql:postgresql:42.7.3")
     compileOnly("org.projectlombok:lombok:1.18.24")
-
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
