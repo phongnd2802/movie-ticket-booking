060d7362a65487ba8b3d6d148d316832cfb41113
phongnd2802 <duyphong02802@gmail.com>
2025-05-05 22:39:57 +0700

    completed auth module

--- server/src/main/java/com/backend/movieticketbooking/dtos/show/response/MovieGetShowResponse.java
+++ server/src/main/java/com/backend/movieticketbooking/dtos/show/response/MovieGetShowResponse.java
@@ -10,6 +10,7 @@ import lombok.experimental.FieldDefaults;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class MovieGetShowResponse {
+    int movieId;
     String movieName;
     int movieAge;
     String movieThumbnail;
